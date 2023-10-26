4f02bc1cf32b4ec07b7a99eb044b2d2a56cde908
Samuel Dervishi <samueldervishi02@gmail.com>
2023-10-26 02:45:14 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "./App.css";
+import awsmobile from "./aws-exports";
+import { Amplify } from "aws-amplify";
+import Dashboard from "./page/dashboard";
+
+Amplify.configure(awsmobile);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dashboard />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
---
+++ src/page/dashboard.js
@@ -0,0 +1,112 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Dashboard.css";
+
+function Dashboard() {
+  const [reports, setReports] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchReports();
+  }, []);
+
+  const fetchReports = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        "https://fxb8z0anl0.execute-api.eu-west-3.amazonaws.com/prod/all-reports"
+      );
+      setReports(response.data.reports);
+      setLoading(false);
+    } catch (error) {
+      console.error("Failed to fetch reports:", error);
+      setError(error);
+      setLoading(false);
+    }
+  };
+
+  const deleteReport = async (reportId) => {
+    try {
+      await axios.delete(
+        `https://fxb8z0anl0.execute-api.eu-west-3.amazonaws.com/prod/delete-report/${reportId}`
+      );
+      console.log("Report deleted successfully");
+      window.location.reload();
+    } catch (error) {
+      console.error("Failed to delete the report:", error);
+    }
+  };
+
+  const renderReports = () => {
+    if (loading) {
+      return (
+        <tr>
+          <td colSpan="5">Loading...</td>
+        </tr>
+      );
+    }
+
+    if (error) {
+      return (
+        <tr>
+          <td colSpan="5">Error: {error.message}</td>
+        </tr>
+      );
+    }
+
+    if (!reports || reports.length === 0) {
+      return (
+        <tr>
+          <td colSpan="5">No reports available.</td>
+        </tr>
+      );
+    }
+
+    return reports.map((report) => (
+      <tr key={report._id} className={report.deleted ? "deleted-report" : ""}>
+        <td>{report.reportedBy?.name}</td>
+        <td>{report.userBeingReported?.companyName}</td>
+        <td>{report.reportReason}</td>
+        <td>{new Date(report.date).toLocaleDateString()}</td>
+        <td>
+          {!report.deleted && (
+            <button
+              className="delete-button"
+              onClick={() => deleteReport(report._id)}
+            >
+              Delete
+            </button>
+          )}
+        </td>
+      </tr>
+    ));
+  };
+
+  return (
+    <div className="dashboard-container">
+      <h1>Admin Dashboard</h1>
+      <button className="fetch-button" onClick={fetchReports}>
+        Get Reports
+      </button>
+      {loading ? (
+        <div className="loader">Loading...</div>
+      ) : (
+        <table className="report-table">
+          <thead>
+            <tr>
+              <th>Reported By</th>
+              <th>Company Reported</th>
+              <th>Report Reason</th>
+              <th>Date of Report</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>{renderReports()}</tbody>
+        </table>
+      )}
+    </div>
+  );
+}
+
+export default Dashboard;
